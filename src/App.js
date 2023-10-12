b3b1cf217d5cb5ee9291ac67243e1db78b1c1557
Niranjanmr2003 <niranjan.mr2021a@vitstudent.ac.in>
2023-10-12 15:47:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [text, setText] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+  useEffect(() => {
+    const words = text.split(' ');
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+  }, [text]);
+
+  return (
+    <div className='container'>
+      <h1>Responsive Paragraph Word Counter</h1>
+      <div>
+        <textarea
+          value={text}
+          onChange={changeHandler}
+        ></textarea>
+
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
